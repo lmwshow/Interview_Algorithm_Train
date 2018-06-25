7e5650fc458ed8880f15bc2417aeeb12971127bd
minGW <397546139@qq.com>
2018-06-25 09:49:13 +0800

    2018/6/23_hihocoder_赛题整理图论类_day3_连通性割点割边

--- hihocoder/src/hihocoder/图论/连通性一_割边与割点minGW.java
+++ hihocoder/src/hihocoder/图论/连通性一_割边与割点minGW.java
@@ -1,15 +1,17 @@
 package hihocoder.图论;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 /**
- * @Auther: minGW
- * @Date: 2018/6/22 08:49
- * @Description: https://blog.csdn.net/jhgkjhg_ugtdk77/article/details/47166809
+
  *
+ * @Auther: minGW
+ * @Date: 2018/6/22 19:26
+ * @Description
  * https://blog.csdn.net/spark__s/article/details/69525645
  *
  * 割边：在连通图中，删除了连通图的某条边后，图不再连通。这样的边被称为割边，也叫做桥。
@@ -21,7 +23,7 @@ import java.util.*;
  * 用dfn[u]记录节点u在DFS过程中被遍历到的次序号，low[u]记录节点u或u的子树通过非父子边追溯到最早的祖先节点（即DFS次序号最小），那么low[u]的计算过程如下：
  *
  *      low[u] = min(low[u]+low[v])    s.t. (u,v)为树边
- *      low[u] = min(low[u],dfn[v])    s.t. (u,v)为回边且v不为u的父亲节点
+ *      low[u] = min(low[u],dfn[v])    s.t. (u,v)为回边且v不为u的父亲节点，因为low表示通过非树边到达的最早祖先
  *
  * 使用Tarjan算法
  *
@@ -32,16 +34,18 @@ import java.util.*;
 
 2.  u不是T的根且存在u的某个儿子w，使得从w或者w的后代没有边连回u的祖先（注意，不是连回u本身）。
  */
-public class 连通性一_割边与割点 {
+public class 连通性一_割边与割点minGW {
 
     static int maxN = 20005;
     static int[] dfn = new int[maxN];
     static int[] low = new int[maxN];
-    static int n,m,counter;
+    static int[] have = new int[maxN];              //标记是否已经加入
+    static int[] parent = new int[maxN];
+    static int[] visit = new int[maxN];             //标记是否已经访问
+    static int[] ansNode = new int[maxN];
+    static int n,m,counter,anscount;
     static Graph graph;
-    static Set<Integer> set;
     static List<Edge> ans;
-    static boolean judge;
 
     static class Vertex {
         public final int index;
@@ -122,39 +126,29 @@ public class 连通性一_割边与割点 {
             graph.addEdge(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]));
         }
 
-        set = new HashSet<>();
         ans = new ArrayList<>();
 
-        tarjan(1,1);//如果已经确定是连通图，就这样写
+        tarjan(1);//如果已经确定是连通图，就这样写
         /*
         否则要这样
         for(int i = 1; i <= n; i++) {
-            if(dfn[i] == 0) tarjan(1, -1);
+            if(dfn[i] == 0) tarjan(1);
         }
         */
 
-        if (!judge) System.out.println("Null");
+        if (anscount == 0) System.out.println("Null");
         else {
 
-            //点这里需要排序
-            List<Integer> list = new ArrayList<>(set);
-            Collections.sort(list);
-            Iterator iterator = list.iterator();
-            boolean first = true;
-            while (iterator.hasNext()) {
-                if (first)
-                {
-                    System.out.print(iterator.next());
-                    first = !first;
-                }
-                else
-                    System.out.print(" " + iterator.next());
-            }
+            Arrays.sort(ansNode,0,anscount);
+            System.out.print(ansNode[0]);
+
+            for (int i = 1; i < anscount;i++)
+                System.out.print(" " + ansNode[i]);
 
             System.out.println();
         }
 
-        
+
         ans.sort((e1,e2)->{
             if (e1.source.index == e2.source.index)
                 return e1.target.index - e2.target.index;
@@ -169,34 +163,42 @@ public class 连通性一_割边与割点 {
             System.out.println(e.source.index + " " + e.target.index);
     }
 
-    private static void tarjan(int u, int father) {
+    private static void tarjan(int u) {
 
         dfn[u] = low[u] = ++ counter;
         int children = 0;       //计算u的儿子节点个数
+        visit[u] = 1;
         List<Edge> edges = graph.getVertex(u).neighbours;
         for (int i = 0 ; i < edges.size();i++)
         {
             int v = edges.get(i).target.index;
-            if (v == father) continue;
 
-            if (dfn[v] == 0)
+            //未访问
+            if (visit[v] == 0)
             {
                 children++;
-                tarjan(v,u);
+                parent[v] = u;
+                tarjan(v);
                 low[u] = Math.min(low[u],low[v]);
 
-                //判断是否割点，点可能进入多次，所以用set方便
-                if ((father == u && children > 1) || (father!=u && low[v] >= dfn[u]))
+                if ((parent[u] == 0 && children > 1) || (parent[u]!=0 && low[v] >= dfn[u]))
                 {
-                    set.add(u);
-                    judge = true;
+                    if (have[u] == 0)
+                    {
+                        ansNode[anscount++] = u;
+                        have[u] = 1;
+                    }
+
                 }
 
                 //判断是否割边(桥)
                 if (low[v] > dfn[u])
                     ans.add(new Edge(new Vertex(Math.min(v,u)),new Vertex(Math.max(v,u))));
 
-            }else   //不需判断是否在栈中
+            }
+            //关键点
+            //已访问，说明是回边   搞明白low的定义，记录节点u或u的子树通过非父子边追溯到最早的祖先节点，有这定义，才有上面对割点割边的判断，所以需要v不是u的父亲节点
+            else if (v != parent[u])
                 low[u] = Math.min(low[u],dfn[v]);
 
 
